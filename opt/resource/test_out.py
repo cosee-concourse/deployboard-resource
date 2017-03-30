632e7db12657f1b19821a0acc36919d3fb4133a8
Felix Weirich <felix.weirich@hotmail.de>
2017-03-30 18:09:43 +0200

    Added Tests

--- opt/resource/test_out.py
+++ opt/resource/test_out.py
@@ -6,7 +6,7 @@ from concourse_common import testutil
 
 class TestOutput(unittest.TestCase):
 
-    def test_json_output_wrong_command(self):
+    def test_json_output_wrong_source(self):
         testutil.put_stdin(json.dumps({"source": {"SLACK_BOT_TOKEN": "test"}, "params": {"version": "1.1.0",
                                                                                          "command": "1.1.0",
                                                                                          "channel": "sdfswfew",
@@ -14,12 +14,33 @@ class TestOutput(unittest.TestCase):
                                                                                          "pipeline_step": "1.1.0"}}))
         self.assertEquals(out.execute("/"), -1)
 
-    def test_json_output_no_pipeline_step(self):
+    def test_json_output_typo(self):
         testutil.put_stdin(json.dumps({"source": {"AWS_ACCESS_KEY": "test", "AWS_SECRET_KEY": "test"},
                                        "params": {
                                        "version": "1.1.0",
                                        "bucket": "failure",
                                        "direcory": "1.1.0"}}))
+        self.assertEquals(out.execute("/"), -1)
+
+    def test_json_output_no_bucket(self):
+        testutil.put_stdin(json.dumps({"source": {"AWS_ACCESS_KEY": "test", "AWS_SECRET_KEY": "test"},
+                                       "params": {
+                                       "bucket": "failure",
+                                       "directory": "1.1.0"}}))
+        self.assertEquals(out.execute("/"), -1)
+
+    def test_json_output_no_version(self):
+        testutil.put_stdin(json.dumps({"source": {"AWS_ACCESS_KEY": "test", "AWS_SECRET_KEY": "test"},
+                                       "params": {
+                                           "bucket": "failure",
+                                           "directory": "1.1.0"}}))
+        self.assertEquals(out.execute("/"), -1)
+
+    def test_json_output_missing_source(self):
+        testutil.put_stdin(json.dumps({"params": {
+                                           "version": "1.1.0",
+                                           "bucket": "failure",
+                                           "directory": "1.1.0"}}))
         self.assertEquals(out.execute("/"), -1)
 
 
